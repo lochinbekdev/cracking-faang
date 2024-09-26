981a5fbf69ec8ac7f56eede579ce2798b74b3dc2
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-09-26 15:50:43 +0500

    best practice

--- leetcode/remove_element.py
+++ leetcode/remove_element.py
@@ -9,9 +9,23 @@ def remove_element(nums, val:int):
             k+=1
         else:
             r_nums.append('_')
-    
     return f'key: {k}',f'nums:{r_nums}' 
 
 nums=[3,2,2,3]
 val=3
-print(remove_element(nums,val))
+
+
+
+# class Solution:
+def removeElement( nums: list[int], val: int) -> int:
+    i = 0
+
+    for num in nums:
+      if num != val:
+        nums[i] = num
+        i += 1
+
+    return i
+
+print(removeElement(nums,val))
+
